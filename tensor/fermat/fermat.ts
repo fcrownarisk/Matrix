e4a3fbebcad9a8d2d578117652c8a62478d80545
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-25 13:13:08 +0800

    Update fermat.ts

--- tensor/fermat/fermat.ts
+++ tensor/fermat/fermat.ts
@@ -9,8 +9,5 @@
  import './tensor/midztensor'
  void function fermat() {
     void ['BIgXtensor' + `midxtensor` + 'smallxtensor`===`BigYtensor`+`midytensor`+`smalltensor`=== `BigYtensor + smallytensor + smallztensor']
-    void ['BIgXtensor - smallxtensor' + 'BigYtensor - smallytensor' +'BigZtensor - smallztensor']
-    void ['midxtensor'+'midytensor'+'midztensor']
-    void ['BigXtensor * BigYtensor * BigZtensor'+'smallxtensor%smallytensor%mallztensor'==='0']
     return fermat
 }
